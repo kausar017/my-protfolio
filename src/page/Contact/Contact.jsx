83fd631bede00bfd50817d2260ac22a1e27e32f9
kausar mia <mdkousarmia71@gmail.com>
2025-01-04 22:48:45 +0600

    fade animation fixt and email sending finxt

--- src/page/Contact/Contact.jsx
+++ src/page/Contact/Contact.jsx
@@ -2,6 +2,7 @@ import Lottie from "lottie-react";
 import emailLottie from '../../assets/lottiFile/Email.json';
 import emailjs from 'emailjs-com';
 import { useRef } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 import { Fade } from "react-awesome-reveal";
 
 const Contact = () => {
@@ -18,12 +19,12 @@ const Contact = () => {
         )
             .then((result) => {
                 console.log('Email sent:', result.text);
-                alert('Your message has been sent successfully!');
+                toast.success('Your message has been sent successfully!');
+                form.current.reset();
             }, (error) => {
                 console.error('Error sending email:', error.text);
-                alert('Failed to send the message. Please try again.');
+                toast.error('Failed to send the message. Please try again.');
             });
-        emailjs.sendto()
     };
 
     return (
@@ -35,57 +36,64 @@ const Contact = () => {
                 </div>
                 <div className="flex justify-around items-center p-2 my-10">
                     <div className="text-center">
-                        <Fade delay={10} duration={2000} direction='left'>
+                        <Fade delay={10} duration={3000} direction='left'>
                             <Lottie className="w-full max-w-96" animationData={emailLottie}></Lottie>
                         </Fade>
                     </div>
                     <div className="card w-full max-w-xl shrink-0">
-                        <Fade delay={10} duration={2000} direction='right'>
-                            <form
-                                ref={form}
-                                onSubmit={sendEmail}
-                                className="card-body bg-black/20 rounded-md border backdrop-blur-lg shadow-lg"
-                            >
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text text-white">Name</span>
-                                    </label>
-                                    <input
-                                        type="text"
-                                        name="user_name"
-                                        placeholder="Your name"
-                                        className="input input-bordered"
-                                        required
-                                    />
-                                </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text text-white">Email</span>
-                                    </label>
-                                    <input
-                                        type="email"
-                                        name="user_email"
-                                        placeholder="Your email"
-                                        className="input input-bordered"
-                                        required
-                                    />
-                                </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text text-white">Message</span>
-                                    </label>
-                                    <textarea
-                                        name="message"
-                                        className="textarea textarea-bordered h-24"
-                                        placeholder="Please type your message"
-                                        required
-                                    ></textarea>
-                                </div>
-                                <div className="form-control mt-6">
-                                    <button className="btn btn-outline text-white hover:bg-white/30">Send</button>
-                                </div>
-                            </form>
+
+                        <Fade delay={10} duration={3000} direction='right'>
+
+                            <div >
+                                <form
+                                    ref={form}
+                                    onSubmit={sendEmail}
+                                    className="card-body bg-black/20 rounded-md border backdrop-blur-lg shadow-lg"
+                                >
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text text-white">Name</span>
+                                        </label>
+                                        <input
+                                            type="text"
+                                            name="user_name"
+                                            placeholder="Your name"
+                                            className="input input-bordered"
+                                            required
+                                        />
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text text-white">Email</span>
+                                        </label>
+                                        <input
+                                            type="email"
+                                            name="user_email"
+                                            placeholder="Your email"
+                                            className="input input-bordered"
+                                            required
+                                        />
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text text-white">Message</span>
+                                        </label>
+                                        <textarea
+                                            name="message"
+                                            className="textarea textarea-bordered h-24"
+                                            placeholder="Please type your message"
+                                            required
+                                        ></textarea>
+                                    </div>
+                                    <div className="form-control mt-6">
+                                        <button className="btn btn-outline text-white hover:bg-white/30">Send</button>
+                                        <ToastContainer />
+                                    </div>
+                                </form>
+
+                            </div>
                         </Fade>
+
                     </div>
                 </div>
             </div>
